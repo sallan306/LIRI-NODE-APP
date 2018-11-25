a7b9b80ddef6d297c13cf3340efe54fbf52e37e6
Scott Allan <sallan306@gmail.com>
2018-11-25 16:03:46 -0600

    initialized a bunch of files, trying to get liri to return functions

---
+++ liri.js
@@ -0,0 +1,18 @@
+// var dotenv  = require("dotenv").config();
+var Spotify = require('node-spotify-api');
+
+var input = process.argv[3]
+
+function concertThis() {
+   var HTML = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    console.log("concertThis")
+}
+function spotifyThisSong() {
+
+}
+function movieThis() {
+
+}
+function doWhatItSays() {
+
+}
